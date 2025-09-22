1024ad6df51c4baf02dca9fcd1499d10f1ae6d22
Osamu Tsuzuki <regulus@OsamunoMac-mini.local>
2025-09-22 18:39:44 +0900

    Update wasm build and UI changes

--- main.js
+++ main.js
@@ -195,7 +195,7 @@ if ("auto" in mainConf) {
         }
 
         // 何かキーが押されたら、自動パンを止める
-        if (autoMode) {
+        if (ev.key === "h" || ev.key == 'l') {
             // console.log("[DEBUG] 自動パン停止: 任意キー", ev.key);
             stopAutoScroll();
             return;
